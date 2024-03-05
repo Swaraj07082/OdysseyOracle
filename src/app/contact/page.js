220f3168d7a8ca6c9dc6ec81135b9122bd094cbf
Swaraj <swarajmali072004@gmail.com>
2024-03-05 21:58:48 +0530

    Contact Us page completed

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,80 +1,49 @@
-import { Avatar } from '@mui/material'
-import React from 'react'
-import Image from 'next/image'
-import linkedin from '../../../public/linkedin.png'
-import twitter from '../../../public/twitter.png'
-import github from '../../../public/github.png'
+"use client";
+import React from "react";
+import Image from "next/image";
+import linkedin from "../../../public/linkedin.png";
+import twitter from "../../../public/twitter.png";
+import github from "../../../public/github.png";
 
+import Link from "next/link";
 
 export default function page() {
   return (
     <>
-<div className='flex flex-col gap-y-12'>
-       
+      <div className="flex gap-x-20 items-center justify-center h-[500px]">
+        <div>
+          <Link href="https://www.linkedin.com/in/swaraj-mali-567070254/">
+            <Image
+              src={linkedin}
+              height={150}
+              width={150}
+              className="mix-blend-multiply"
+            />
+          </Link>
+        </div>
 
+        <div>
+          <Link href="https://twitter.com/SwarajM07">
+            <Image
+              src={twitter}
+              height={150}
+              width={150}
+              className="mix-blend-multiply"
+            />
+          </Link>
+        </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-    <div>
-<p>Contact Us</p>
-    </div>
-    
-<div className='flex'>
-
-
-<div>
-
-    <Image src={linkedin}  className='mix-blend-multiply'/>
-</div>
-
-<div>
-<Image src={twitter} className='mix-blend-multiply'/>
-
-
-</div>
-
-<div>
-
-<Image src={github} className='mix-blend-multiply'/>
-
-</div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
+        <div>
+          <Link href="https://github.com/Swaraj07082">
+            <Image
+              src={github}
+              height={150}
+              width={150}
+              className="mix-blend-multiply"
+            />
+          </Link>
+        </div>
+      </div>
     </>
-      
-  )
+  );
 }
